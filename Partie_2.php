f455e8605d5a60757d1a290c75fc97602ed8bacf
Caffin <caffinmanon@gmail.com>
2023-12-06 09:55:09 +0100

    finish

--- Partie_2.php
+++ Partie_2.php
@@ -52,25 +52,25 @@ switch($richter){
         break;
     case 3:
         echo "<p>Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.</p>";
-        break
+        break;
     case 4:
         echo "<p>Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.</p>";
-        break
+        break;
     case 5: 
         echo "<p>Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.</p>";
-        break
+        break;
     case 6:
         echo "<p>Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.</p>";
-        break
+        break;
     case 7:
         echo "<p>Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.</p>";
-        break
+        break;
     case 8:
         echo "<p>Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.</p>";
-        break
+        break;
     case 9:
         echo "<p>Séisme capable de tout détruire sur une très vaste zone.</p>";
-        break
+        break;
 }
 
 // Exercice 5
