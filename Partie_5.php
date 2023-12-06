4ffd7ea428dd3625072d733c606d4a93e07fcbe5
Caffin <caffinmanon@gmail.com>
2023-12-06 16:53:38 +0100

    dernier jet

---
+++ Partie_5.php
@@ -0,0 +1,91 @@
+<?php
+// Exercice 1 
+// $months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembe', 'decembre'];
+
+// Exercice 2
+// $months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembe', 'decembre'];
+
+// echo $months [2];
+
+// Exercice 3
+// $months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembe', 'decembre'];
+
+// echo $months [5];
+
+// Exercice 4
+// $months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembe', 'decembre'];
+
+// $months[7] = 'août';
+
+// echo $months [7];
+// var_dump ($months);
+
+// Exercice 5
+// $hautdeFrance = [
+//     'Ainse' => 02,
+//     'Nord' => 59,
+//     'Oise' => 60,
+//     'Pas-de-Calais' => 62,
+//     'Somme' => 80,
+// ];
+
+// var_dump ($hautdeFrance);
+
+// Exercice 6
+// $hautdeFrance = [
+//     'Ainse' => 02,
+//     'Nord' => 59,
+//     'Oise' => 60,
+//     'Pas-de-Calais' => 62,
+//     'Somme' => 80,
+// ];
+
+// echo $hautdeFrance ['Nord'];
+
+// Exercice 7 
+// $hautdeFrance = [
+//     'Ainse' => 02,
+//     'Nord' => 59,
+//     'Oise' => 60,
+//     'Pas-de-Calais' => 62,
+//     'Somme' => 80,
+// ];
+
+// var_dump ($hautdeFrance);
+// $hautdeFrance = ['Reims' => 51];
+// var_dump ($hautdeFrance);
+
+// Exercice 8
+// $months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembe', 'decembre'];
+
+// foreach ($months as $month){
+// echo $month;
+// }
+
+// Exercice 9
+// $departements = [
+//     'Ainse' => 02,
+//     'Nord' => 59,
+//     'Oise' => 60,
+//     'Pas-de-Calais' => 62,
+//     'Somme' => 80,
+// ];
+
+// foreach($departements as $departement){
+//     echo$departement;
+// }
+
+// Exercice 10
+// $departements = [
+//      02 => 'Ainse',
+//      59 => 'Nord',
+//      60 => 'Oise',
+//      62 => 'Pas-de-Calais',
+//      80 => 'Somme',
+// ];
+
+// foreach ($departements as $key => $departement) {
+//     echo "<p>Le département " . $departement . " a le numéro " . $key . "</p>";
+// }
+
+?>
