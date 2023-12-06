3b7d2aab34f50a5a46a16aa83ac1604b36725ae8
Caffin <caffinmanon@gmail.com>
2023-12-06 14:47:40 +0100

    finish

--- Partie_4.php
+++ Partie_4.php
@@ -1,5 +1,95 @@
 <?php
 
+// // Exercice 1
+// function retourneTrue() {
+//     return true;
+// }
+
+// // Appel de la fonction
+// $resultat = retourneTrue();
+
+// // Affichage du résultat
+// var_dump($resultat); // Cela affichera "bool(true)"
+
+// // Exercice 2 :
+// $alexis = "alexis";
+// $turan = "turan";
+
+// function chaine($prenom1,$prenom2) {
+
+//     return $prenom1 . " " . $prenom2;
+// }
+
+// echo chaine($alexis, $turan);
+
+// Exercice 3
+// function chaine($prenom1,$prenom2) {
+
+//     return $prenom1." ". $prenom2;
+// }
+// echo chaine("bonjour","aurevoir");
+
+// Exercice 4
+// function nombres ($premierNombre, $secondNombre){
+//     if ($premierNombre > $secondNombre){
+//     echo "<p>Le premier nombre est plus grand.</p>";
+// } 
+// elseif ($premierNombre < $secondNombre){
+//     echo "<p>Le second nombre est plus grand.</p>";
+// }
+// else {
+//     echo "<p>Les deux nombres sont identiques.</p>";
+// }
+// }
+// nombres(10,20);
+// nombres(20,10);
+// nombres(10,10);
+
+// Exercice 5
+
+// function blaBla($unNombre, $chaineCaractere){
+//     return $unNombre." ". $chaineCaractere;
+// }
+//     echo blaBla(10,"Yvotte");
+
+// Exercice 6
+// function blablaBla($leNom, $lePrenom, $age){
+//     return "<p>Bonjour " . $leNom ." ". $lePrenom . ", tu as" . " " . $age."</p>";
+// }
+
+// echo blablaBla("Caffin", "Manon", 33)
+
+// Exercice 7
+// function personne($age, $gender){
+
+// if ($age >= 18 && $gender === "Femme") {
+//     echo "Vous êtes une femme et vous êtes majeur.";
+// } 
+// elseif ($age >= 18 && $gender === "Homme") {
+//     echo "Vous êtes un homme et vous êtes majeur.";
+// }
+// elseif ($age < 18 && $gender === "Femme") {
+//     echo "Vous êtes une femme et vous êtes mineur.";
+// }
+// elseif ($age < 18 && $gender === "Homme") {
+//     echo "Vous êtes un homme et vous êtes mineur.";
+// }
+// }
+
+// personne(65, "Femme");
+// personne(16, "Femme");
+// personne(65, "Homme");
+// personne(16, "Homme");
+
+// Exercice 8
+// function sum($premierNombre, $secondNombre, $troisiemeNombre){
+//     return ($premierNombre + $secondNombre + $troisiemeNombre);
+// }
+
+// echo sum (1, 2, 3);
+
+
+
 
 
 
