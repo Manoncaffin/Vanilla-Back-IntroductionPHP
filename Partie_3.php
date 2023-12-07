842afa8ae7073de95fc4f901801abe7cd2f66e9f
Caffin <caffinmanon@gmail.com>
2023-12-07 09:21:38 +0100

    finish

--- Partie_3.php
+++ Partie_3.php
@@ -1,57 +1,93 @@
 <?php
 
-// Exercice 1
-for ($i = 0 ; $i < 11 ; $i ++){
-    echo $i;
-}
+// // Exercice 1
+// for ($i = 0 ; $i < 11 ; $i ++){
+//     echo $i;
+// }
 
-// Exercice 2
-$i = 0;
-$b = 85;
+// // Exercice 2
+// $i = 0;
+// $b = 85;
 
-while ($i <= 20){
-    echo "<p>". ($i * $b) . "</p>";
-    $i++;
-}
+// while ($i <= 20){
+//     echo "<p>". ($i * $b) . "</p>";
+//     $i++;
+// }
 
-// Exercice 3
-$a = 100;
-$b = 40;
+// // OU
+// for ($i; $b <= 20; $i++) {
+//     echo "<p>" . $i * $b . "</p>";
+// }
 
-while ($a <= 10){
-    echo "<p>". ($a * $b) . "</p>";
-    $a--;
-}
+// // Exercice 3
+// $a = 100;
+// $b = 40;
 
-// Exercice 4
-for( $i = 1; $i <=10;$i =  $i/2 +$i )
-echo $i."     ";
+// while ($a <= 10){
+//     echo "<p>" . ($a * $b) . "</p>";
+//     $a--;
+// }
 
-// Exercice 5
-$a = 0;
-while ($a <= 14){
-    echo "<p>". ($a += 1) . "</p>";
-    echo "<p>On y arrive presque.</p>";
-}
+// // OU
+// $number1 = 100;
+// $number2 = 50;
 
-// Exercice 6
-$a = 21;
-while ($a > 0){
-    echo "<p>". ($a -= 1) . "</p>";
-    echo "<p>C'est presque bon.</p>";
-}
+// for ($number1, $number > 10; $number--) {
+//     echo "<p>" . $number1 * $number2 . "</p>";
+// }
 
-// Exercice 7
-$a = 0;
-while ($a <= 100){
-    echo "<p>". ($a += 15) . "</p>";
-    echo "<p>On tient le bon bout.</p>";
-}
+// // Exercice 4
+// for( $i = 1; $i <=10;$i =  $i/2 +$i ) 
+// echo "<p>" . $i ."</p>";
 
-// Exercice 8
-$a = 200;
-while ($a > 0){
-    echo "<p>". ($a -= 12) . "</p>";
-    echo "<p>Enfin !!!!</p>";
-}
+// // Exercice 5
+// $a = 0;
+// while ($a <= 14){
+//     echo "<p>". ($a += 1) . "</p>";
+//     echo "<p>On y arrive presque.</p>";
+// }
+
+// // OU
+// for($i = 1, $i <= 15; $i++) {
+//     echo "<p>" . $i . " On y arrive presque.</p>";
+// }
+
+// // Exercice 6
+// $a = 21;
+// while ($a > 0){
+//     echo "<p>". ($a -= 1) . "</p>";
+//     echo "<p>C'est presque bon.</p>";
+// }
+
+// OU
+// fo($i = 20, $i >= 0; $i--) {
+//     echo "<p>C'est presque bon.</p>";
+// }
+
+// // Exercice 7
+// $a = 0;
+// while ($a <= 100){
+//     echo "<p>". ($a += 15) . "</p>";
+//     echo "<p>On tient le bon bout.</p>";
+// }
+
+// OU 
+
+// for($i = 1, $i <= 100; $i+= 15) {
+//     echo "<p>On tient le bon bout.</p>";
+// }
+
+// // Exercice 8
+// $a = 200;
+// while ($a > 0){
+//     echo "<p>". ($a -= 12) . "</p>";
+//     echo "<p>Enfin !!!!</p>";
+// }
+
+// OU
+
+// for($i = 200, $i >= 0; $i -= 12) {
+//     echo "<p>Enfin !!!!</p>";
+// }
+
 ?>
